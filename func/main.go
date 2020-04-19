927c2d653ae9bfd9711ff85c3a2c0228b33be892
yasu <568200065@qq.com>
2020-04-19 23:08:29 +0900

    添加 Go语言基础-函数

---
+++ func/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// 函数
+func sayHello(){
+	fmt.Println("Hello,world~")
+}
+
+// 定义一个带参数的函数
+func sayHelloTo(name string){
+	fmt.Println("hello", name)
+}
+
+// 定义接收多个参数的函数 
+//如果给返回值声明了一个变量，则函数体中直接使用这个变量即可
+//如果返回值没有声明变量，只声明了类型，那么函数体中需要声明才能使用
+func intSum(a int, b int) (int) {   
+	ret := a + b
+	return ret
+}
+
+func main(){
+	//函数调用
+	sayHello()
+	sayHelloTo("Tom")
+
+	ret := intSum(2, 3)
+	fmt.Println(ret)
+
+}
+
