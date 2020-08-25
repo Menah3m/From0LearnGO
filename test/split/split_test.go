149d0d171416099818f778628c2b03bd677d44e9
yasu <568200065@qq.com>
2020-08-25 22:51:36 +0900

    更新  Go语言基础-单元测试之测试组

--- test/split/split_test.go
+++ test/split/split_test.go
@@ -7,14 +7,44 @@ import (
 
 // 测试
 
+// func TestSplit(t *testing.T){
+// 	got := Split("我爱你", "爱")
+// 	want := []string{"我", "你"}
+// 	// got := Split("a:b", ":")
+// 	// want := []string{"a", "b"}
+	
+// 	if !reflect.DeepEqual(got, want){
+// 		t.Errorf("want:%v  got:%v", want, got)
+	
+// 	}
+// }
+
+
 func TestSplit(t *testing.T){
-	got := Split("我爱你", "爱")
-	want := []string{"我", "你"}
-	// got := Split("a:b", ":")
-	// want := []string{"a", "b"}
-	
-	if !reflect.DeepEqual(got, want){
-		t.Errorf("want:%v  got:%v", want, got)
-	
+	type test struct{
+		input string
+		sep string
+		want []string
 	}
+
+	tests := map[string]test{
+		"simple":test{ 
+			input: "我爱你",
+			sep:"爱",
+			want:[]string{"我", "你"},
+		},
+		"multi sep":test{ 
+			input: "a:b:c",
+			sep:":",
+			want:[]string{"a", "b", "c"},
+		},
+
+	}
+	for name, tc := range tests{
+		got := Split(tc.input, tc.sep)
+		if !reflect.DeepEqual(got, tc.want){
+			t.Errorf("name:%v failed, want:%v got:%v", name, tc.want, got)
+		}
+	}
+
 }
