d5709dfe818347d62c8fd3688492e9cddebf5e3b
yasu <568200065@qq.com>
2020-09-06 14:26:33 +0900

    更新  Go语言基础-time包

--- time/main.go
+++ time/main.go
@@ -10,6 +10,12 @@ import (
 func main(){
 	now := time.Now() // 得到一个时间对象
 	fmt.Println(now)
+
+	after1hour := now.Add(time.Hour)
+	fmt.Println(after1hour)
+
+	duration := after1hour.Sub(now)
+	fmt.Println(duration)
 	
 	year := now.Year()
 	month := now.Month()
@@ -35,5 +41,34 @@ func main(){
 	
 	n := 5  //type int
 	time.Sleep(time.Duration(n)*time.Second)
-	println("over")
+	fmt.Println("over")
+
+	// 定时器
+	// for tmp := range time.Tick(2 * time.Second) {
+	// 	fmt.Println(tmp)
+	// }
+
+	// 时间格式化 Y     m  d   H  M  S
+	//           2006  01 02  15 04 05
+	ret1 := now.Format("2006.01.02 15:04:05")	
+	fmt.Println(ret1)
+	
+	// 解析 字符串类型的时间
+	timeStr := "2019/08/07 15:00:00"
+	// 1.拿到时区
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil{
+		fmt.Println(err)
+		return 
+	}
+
+	// 2. 根据时区去解析一个字符串类型的时间
+	timeObj, err := time.ParseInLocation("2006-01-02 15:04:05", timeStr, loc)
+	if err != nil{
+		fmt.Printf("parse timeStr failed. err: %v\n", err)
+		return 
+	}
+	fmt.Println(timeObj)
+
+
 }
